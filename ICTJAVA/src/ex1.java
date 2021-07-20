b34ff67b455f389d5797b9cd736add88ddfed2c8
JeongJiWon <jskjw157@gmail.com>
2021-07-20 13:44:42 +0900

    ex1 modify

--- ICTJAVA/src/ex1.java
+++ ICTJAVA/src/ex1.java
@@ -4,7 +4,7 @@ public class ex1 {
 	public static void main(String[] args) {
 		// 2021.07.02. 프로그래밍 첫 수업 -1-
 		
-		System.out.println("이름 : 함상현");
+		System.out.println("이름 : 정지원");
 		System.out.println("");
 		System.out.println("<지원 동기>");
 		System.out.println("코딩에 대한 관심으로 인해 내가 직접 배워서 하고 싶다는 생각이 들어서 입니다.");
